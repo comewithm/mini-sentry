6e39f10c49718a847709636f55e1f89dc340e026
wanghuan1 <wanghuan1@duiba.com.cn>
2023-03-23 22:19:43 +0800

    style(root): 格式化

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   },
   plugins: ["html"],
   extends: [
+    'eslint:recommended',
     "prettier"
   ],
   rules: {
--- src/sdk.ts
+++ src/sdk.ts
@@ -1,40 +1,34 @@
-import {  initClient } from "./client";
-import { IClientOptions } from "interface/options";
-import { BrowserClient } from "integration/client";
-import { BreadCrumb } from "integration/breadcrumb";
-import { mergeIntegrations } from "core/integrations";
-import { IOptions } from "interface";
-import { GlobalHandler } from "integration/globalhandler";
-import { SHOULD_LOG } from "./cons";
+import { initClient } from './client'
+import { IClientOptions } from 'interface/options'
+import { BrowserClient } from 'integration/client'
+import { BreadCrumb } from 'integration/breadcrumb'
+import { mergeIntegrations } from 'core/integrations'
+import { IOptions } from 'interface'
+import { GlobalHandler } from 'integration/globalhandler'
+import { SHOULD_LOG } from './cons'
 
-
-export const defaultIntegrations = [
-    new BreadCrumb(),
-    new GlobalHandler(),
-
-]
+export const defaultIntegrations = [new BreadCrumb(), new GlobalHandler()]
 
 export function init(options: IOptions) {
+  SHOULD_LOG && console.log('init')
 
-    SHOULD_LOG && console.log("init")
+  if (options.defaultIntegrations == undefined) {
+    options.defaultIntegrations = defaultIntegrations
+  }
 
-    if(options.defaultIntegrations == undefined) {
-        options.defaultIntegrations = defaultIntegrations
-    }
-    
-    // 默认值和手动设置值合并
-    const baseOptions:IClientOptions = {
-        ...options,
-        integrations: mergeIntegrations(options)
-    }
+  // 默认值和手动设置值合并
+  const baseOptions: IClientOptions = {
+    ...options,
+    integrations: mergeIntegrations(options),
+  }
 
-    // 实例类，不是抽象类
-    initClient(BrowserClient,  baseOptions)
+  // 实例类，不是抽象类
+  initClient(BrowserClient, baseOptions)
 }
 
 init({
-    performance: true,
-    xhr: true,
-    history: true,
-    fetch: true
+  performance: true,
+  xhr: true,
+  history: true,
+  fetch: true,
 })
